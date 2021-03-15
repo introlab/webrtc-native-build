9df558f6602b892e3bbe08d491d93e702fb95187
Dominic Letourneau <dominic.letourneau@usherbrooke.ca>
2021-03-15 12:06:22 -0400

    Added flags according to architecture

--- 3rdParty/webrtc_native/generate_ninja_files.bash
+++ 3rdParty/webrtc_native/generate_ninja_files.bash
@@ -1,15 +1,45 @@
 #!/bin/bash
 
-sysroot=$1
+architecture=$1
+sysroot=$2
 
-echo "--------------------------- $sysroot"
+echo "Architecture: $architecture"
+echo "Sysroot: $sysroot"
 
-a=arm64
 extras=""
+extra_flags=""
 
-common_flags="is_clang=false is_component_build=false treat_warnings_as_errors=false fatal_linker_warnings=false use_gio=false use_rtti=true use_custom_libcxx=false use_custom_libcxx_for_host=false rtc_enable_protobuf=false rtc_include_tests=false rtc_use_h264=true proprietary_codecs=true target_os=\"linux\" target_cpu=\"$a\"$extras enable_iterator_debugging=false rtc_build_examples=false rtc_use_pipewire=false use_sysroot=true sysroot=\"$sysroot\""
+case "$architecture" in
+	i?86)
+		a=x86
+		
+		extras=""
+		extra_flags=""
+	;;
+	x86_64|amd64)
+		a=x64
+		extras=""
+		extra_flags=""
+	;;
+	armhf|armv*)
+		a=arm
+		extras=" arm_float_abi=\"hard\""
+		extra_flags="use_sysroot=true sysroot=\"$sysroot\""
+	;;
+	arm64|aarch64)
+		a=arm64
+		extras=""
+		extra_flags="use_sysroot=true sysroot=\"$sysroot\""
+	;;
+	*)
+		echo>&2 "WARNING: Unknown target platform: $a, continuing anyway"
+	;;
+esac
 
 
-gn gen out/Debug --args="is_debug=true $common_flags"
-gn gen out/Release --args="is_debug=false $common_flags"
+common_flags="is_clang=false is_component_build=false treat_warnings_as_errors=false fatal_linker_warnings=false use_gio=false use_rtti=true use_custom_libcxx=false use_custom_libcxx_for_host=false rtc_enable_protobuf=false rtc_include_tests=false rtc_use_h264=true proprietary_codecs=true target_os=\"linux\" target_cpu=\"$a\"$extras enable_iterator_debugging=false rtc_build_examples=false rtc_use_pipewire=false"
+
+
+gn gen out/Debug --args="is_debug=true $common_flags $extra_flags"
+gn gen out/Release --args="is_debug=false $common_flags $extra_flags"
 

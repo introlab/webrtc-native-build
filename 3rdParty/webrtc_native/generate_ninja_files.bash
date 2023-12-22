75deda7b241c880f30ff1fb26d8986df027d15db
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2023-12-22 09:27:02 -0500

    Trying rtc_build_tools=true for arm64

--- 3rdParty/webrtc_native/generate_ninja_files.bash
+++ 3rdParty/webrtc_native/generate_ninja_files.bash
@@ -33,7 +33,7 @@ case "$architecture" in
 	arm64|aarch64)
 		a=arm64
 		extras=""
-		extra_flags="is_clang=true use_sysroot=true sysroot=\"$sysroot\" target_os=\"linux\" arm_control_flow_integrity=\"none\""
+		extra_flags="rtc_build_tools=true is_clang=true use_sysroot=true sysroot=\"$sysroot\" target_os=\"linux\" arm_control_flow_integrity=\"none\""
 	;;
 	osx64)
 		a=x64

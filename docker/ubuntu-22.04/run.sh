43b42d646dad81623b667da8d1f604a34e489fc7
Dominic Letourneau <dominic.letourneau@usherbrooke.ca>
2024-01-09 08:34:51 -0500

    Added example run from docker

---
+++ docker/ubuntu-22.04/run.sh
@@ -0,0 +1,5 @@
+docker buildx build -t webrtc --build-arg BUILD_TYPE=Debug --build-arg BUILD_ARCHITECTURE=arm64 --build-arg BUILD_BRANCH=update-webrtc .
+docker run --rm \
+    --volume $PWD:/data webrtc \
+    ls -l
+#    tar cvzf /data/release.tar.gz ./
